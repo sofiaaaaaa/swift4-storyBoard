3895abf96f396ada5a82602334a9787e82df205c
임지후 <jihooyim@gmail.com>
2018-08-14 13:40:15 +0900

    Initial Commit

---
+++ storyBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  storyBoard
+//
+//  Created by 임지후 on 2018. 8. 14..
+//  Copyright © 2018년 임지후. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
